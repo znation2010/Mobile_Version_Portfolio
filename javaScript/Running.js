b5424352da6f8dbabea58a2a27fd7b4c16537263
Balolebwami <tchasingajacques@gmail.com>
2023-05-26 17:25:37 +0200

    JavaScript Organization
    here I've done to design more than an element , and add some Interaction

--- javaScript/Running.js
+++ javaScript/Running.js
@@ -1,3 +1,10 @@
+// let Navlinking = document.querySelector('.newNavLinks');
+// const Offing = document.getElementById('newCloseTag');
+
+document.querySelectorAll('.newNavLinks').forEach((element) => element.addEventListener('click', () => {
+  document.querySelector('.mobile-menu').style.display = 'none';
+}));
+
 const openNavigation = function openNavHandler() {
   document.querySelector('.mobile-menu').style.display = 'block';
 };
